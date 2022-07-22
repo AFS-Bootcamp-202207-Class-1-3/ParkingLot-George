6b2be7e6b6e5a4b0f4aff45c48e6b42d8516d28c
CHENGE8 <1911270452@qq.com>
2022-07-22 11:53:03 +0800

    feat:implement should_return_parking_ticket_when_park_car_given_parking_lot_and_car

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,21 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+
+    @Test
+    void should_return_parking_ticket_when_park_car_given_parking_lot_and_car() {
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot();
+
+        //when
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        //then
+        assertNotNull(parkingTicket);
+    }
+}
