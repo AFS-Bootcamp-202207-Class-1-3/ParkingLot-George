3aff7e40818dd2af4b99bcafba279e8f33b7c389
chan <1911270452@qq.com>
2022-07-23 00:08:16 +0800

    feat:implement should_return_parked_car_when_parking_boy_fetch_car_given_parking_ticket && fix:should_return_parking_ticket_when_parking_boy_park_given_parking_lot_and_boy_car

--- src/main/java/com/parkinglot/ParkingBoy.java
+++ src/main/java/com/parkinglot/ParkingBoy.java
@@ -2,7 +2,20 @@ package com.parkinglot;
 
 public class ParkingBoy {
 
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public ParkingBoy() {
+    }
+
     public ParkingTicket park(Car car) {
-        return new ParkingTicket(false);
+        return parkingLot.park(car);
+    }
+
+    public Car fetch(Car exceptedCar, ParkingTicket parkingTicket) {
+        return parkingLot.fetch(parkingTicket);
     }
 }
--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -2,14 +2,15 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
     @Test
-    void should_return_parking_ticket_when_parking_boy_park_given_parking_lot_and_boy_car() {
+    void should_return_parking_ticket_when_parking_boy_park_given_parking_lot_and_car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
-        ParkingBoy parkingBoy = new ParkingBoy();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
 
         //when
@@ -17,5 +18,20 @@ public class ParkingBoyTest {
 
         //then
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    void should_return_parked_car_when_parking_boy_fetch_car_given_parking_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car exceptedCar = new Car();
+
+        //when
+        ParkingTicket parkingTicket = parkingBoy.park(exceptedCar);
+        Car car = parkingBoy.fetch(exceptedCar, parkingTicket);
+
+        //then
+        assertEquals(exceptedCar, car);
     }
 }
